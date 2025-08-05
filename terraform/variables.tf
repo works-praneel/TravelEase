df5fbba3aff5cf054f97256a31c193570e99d7b6
works-praneel <works.praneel@gmail.com>
2025-08-05 20:55:51 +0530

    Updated Variable.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,66 +1,46 @@
-variable "aws_region" {
-  description = "AWS region for deployment"
-  type        = string
-  default     = "eu-north-1" # Change to your desired region
+variable "project_name" {
+  default = "travelease"
 }
 
-variable "project_name" {
-  description = "Name of the project"
-  type        = string
-  default     = "TravelEase"
+variable "aws_region" {
+  default = "eu-north-1"
 }
 
 variable "aws_account_id" {
-  description = "AWS Account ID for ECR image repository"
-  type        = string
-  default     = "904233121598"
-}
-
-variable "frontend_image" {
-  description = "Docker image for frontend service"
-  type        = string
-  default     = "904233121598.dkr.ecr.eu-north-1.amazonaws.com/frontend-service:latest"
-}
-
-variable "flight_image" {
-  description = "Docker image for flight service"
-  type        = string
-  default     = "904233121598.dkr.ecr.eu-north-1.amazonaws.com/flight-service:latest"
-}
-
-variable "payment_image" {
-  description = "Docker image for payment service"
-  type        = string
-  default     = "904233121598.dkr.ecr.eu-north-1.amazonaws.com/payment-service:latest"
+  default = "904233121598"
 }
 
 variable "booking_image" {
-  description = "Docker image for booking service"
-  type        = string
-  default     = "904233121598.dkr.ecr.eu-north-1.amazonaws.com/booking-service:latest"
+  type    = string
+  default = "904233121598.dkr.ecr.eu-north-1.amazonaws.com/booking-service:latest"
 }
 
-
-variable "frontend_alb_dns" {
-  description = "DNS name of the Application Load Balancer for the frontend"
-  type        = string
-  default     = "" # Will be passed by Jenkins after ALB creation
+variable "flight_image" {
+  type    = string
+  default = "904233121598.dkr.ecr.eu-north-1.amazonaws.com/flight-service:latest"
 }
 
-variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+variable "payment_image" {
+  type    = string
+  default = "904233121598.dkr.ecr.eu-north-1.amazonaws.com/payment-service:latest"
 }
 
-variable "public_subnet_cidrs" {
-  description = "List of CIDR blocks for public subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+variable "auth_image" {
+  type    = string
+  default = "904233121598.dkr.ecr.eu-north-1.amazonaws.com/auth-service:latest"
 }
 
-variable "private_subnet_cidrs" {
-  description = "List of CIDR blocks for private subnets"
-  type        = list(string)
-  default     = ["10.0.10.0/24", "10.0.11.0/24"]
+variable "user_image" {
+  type    = string
+  default = "904233121598.dkr.ecr.eu-north-1.amazonaws.com/user-service:latest"
+}
+
+variable "frontend_image" {
+  type    = string
+  default = "904233121598.dkr.ecr.eu-north-1.amazonaws.com/frontend:latest"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
 }
